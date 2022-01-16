5174f2b02439843cf936192d3273a300d979bab3
shrikant jadhav <shrikantjadhav20@cse.iiitp.ac.in>
2022-01-16 16:32:34 +0530

    first page done

--- land.js
+++ land.js
@@ -1,11 +1,25 @@
-console.log("hi");
-console.log(window.localStorage.getItem("nteam"));
-console.log(window.localStorage.getItem("nscore"));
+
+
+function zoom() {
+  const butn = document.getElementById("submit") ;
+  butn.style.backgroundColor = "black";
+  butn.style.transform = "scale(50)";
+  butn.textContent = "";
+  
+}
+
 function save() {
-  window.localStorage.setItem("nteam", document.getElementById("nteam").value);
+  window.localStorage.setItem("team1", document.getElementById("team1").value);
+  window.localStorage.setItem("team2", document.getElementById("team2").value);
   window.localStorage.setItem(
     "nscore",
     document.getElementById("nscore").value
   );
-  window.location.href = "main.html";
 }
+
+setInterval(function(){
+  const title = document.getElementById("submit");
+  if(title.style.transform == "scale(50)"){
+    window.location.href = "main.html";
+  }
+},2000)
