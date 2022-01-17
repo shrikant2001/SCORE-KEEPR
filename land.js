834123151937518b9126747422ea7bfb36ccea7b
shrikant jadhav <shrikantjadhav20@cse.iiitp.ac.in>
2022-01-17 09:10:31 +0530

    landing compatiblised

--- land.js
+++ land.js
@@ -1,24 +1,10 @@
 
 
-function zoom() {
-  const butn = document.getElementById("submit") ;
-  butn.style.backgroundColor = "black";
-  butn.style.transform = "scale(50)";
-  butn.textContent = "Let's Go !";
-  
-}
-
 function save() {
   window.localStorage.setItem("team1", document.getElementById("team1").value);
   window.localStorage.setItem("team2", document.getElementById("team2").value);
   window.localStorage.setItem(
     "nscore",
     document.getElementById("nscore").value);
-}
-
-setInterval(function(){
-  const title = document.getElementById("submit");
-  if(title.style.transform == "scale(50)"){
     window.location.href = "main.html";
-  }
-},2000)
+}
